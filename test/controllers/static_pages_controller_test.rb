d17341ec07dee6ac7a17db2e64d0cc72d38cb4dc
Marco Hinz <git@workchoice.de>
2016-04-02 22:38:52 +0200

    Added contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,4 +18,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Workchoice"
   end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Workchoice"
+  end
+  
 end
