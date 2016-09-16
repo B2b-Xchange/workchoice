ecc05b16fc7eae3ea5578c191629612966c4a577
Marco Hinz <git@workchoice.de>
2016-09-16 21:55:41 +0200

    Use ENV variables for Omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,11 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-#  provider :facebook, '232020127155218', '201b7050b52e48a6d879a9544b0d5060',
+#  provider :facebook, ENV['OA_FACEBOOK_ID'], ENV['OA_FACEBOOK_SECRET'],
 #           :scope => 'email,public_profile', :display => 'popup'
-  provider :linkedin, '77wods3i15m22h', '3gs5fGTo86gMD91o',
+  provider :linkedin, ENV['OA_LINKEDIN_ID'], ENV['OA_LINKEDIN_SECRET'],
            :scope => 'r_basicprofile r_emailaddress',
            :fields => ["first-name", "last-name", "email-address"]
-  provider :xing, 'fc81e6bcf4c587baa010', 'fdf432dc28733456e0371b09bd097601d8567a0e'
+  provider :xing, ENV['OA_XING_ID'], ENV['OA_XING_SECRET']
   
 end
