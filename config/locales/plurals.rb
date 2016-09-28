7118eac2618e0b7edfb916da9a65828491a645d7
Marco Hinz <git@workchoice.de>
2016-09-28 23:41:09 +0200

    Just another translations

---
+++ config/locales/plurals.rb
@@ -0,0 +1,16 @@
+
+{:de => 
+  { :i18n => 
+    { :plural => 
+      { :keys => [:one, :other],
+        :rule => lambda { |n| 
+          if n == 1
+            :one
+          else
+            :other 
+          end
+        } 
+      } 
+    } 
+  } 
+}
